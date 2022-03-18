1fad1b93e4372d20907be86cd08afda5ed79268e
Murilo Cavalcanti <78709896+muriloLcavalcanti@users.noreply.github.com>
2022-03-18 14:13:20 -0300

    Simplificando o código
    O compilador do TS entende que:
     Array<"Nome da classe"> = [ ]
    é o mesmo que:
    "Nome da classe"[ ]
    
    E nos casos onde é um array de apenas leitura (não deve ser modificado):
    readonly "Nome da classe"[ ]
    no lugar de:
    ReadonlyArray<"Nome da classe">
    
    Em alguns casos opta-se por manter o código da forma mais verbosa para facilitar a leitura de outras pessoas.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[];
 
     adiciona(negocicao: Negociacao){
         this.negociacoes.push(negocicao);
     }
 
-    lista(): ReadonlyArray<Negociacao>{ 
+    lista(): readonly Negociacao[]{ 
         return this.negociacoes;
         // o tipo ReadonlyArray é identico ao tipo Array, porém exclui os metodos que permitem alterar o arraye mantem apenas os que metodos de leitura.
     }
